77edd911dbcfa7453cdee8ac0eb13bbefe9ddce5
Determinant <tederminant@gmail.com>
2021-01-13 19:45:09 -0500

    add get + delete

--- cedrusdb.go
+++ cedrusdb.go
@@ -3,10 +3,10 @@ package cedrusdb
 // #include <stdlib.h>
 // #include "cedrusdb/db.h"
 import "C"
-import "runtime"
 import (
 	"fmt"
 	"os"
+	"runtime"
 	"unsafe"
 )
 
@@ -60,9 +60,52 @@ func (c Cedrus) Free() {
 	}
 }
 
+//// end Cedrus def
+
+//// begin CedrusValueRef def
+
+// CCedrusValueRef is the C pointer type for a NetAddr object
+type CCedrusValueRef = *C.CedrusValueRef
+type cedrusValueRef struct {
+	inner    CCedrusValueRef
+	autoFree bool
+	freed    bool
+}
+
+type CedrusValueRef = *cedrusValueRef
+
+// CedrusValueRefFromC converts an existing C pointer into a go object. Notice that
+// when the go object does *not* own the resource of the C pointer, so it is
+// only valid to the extent in which the given C pointer is valid. The C memory
+// will not be deallocated when the go object is finalized by GC. This applies
+// to all other "FromC" functions.
+func CedrusValueRefFromC(ptr CCedrusValueRef) CedrusValueRef {
+	return &cedrusValueRef{inner: ptr}
+}
+
+func cedrusValueRefSetFinalizer(res CedrusValueRef, autoFree bool) {
+	res.autoFree = autoFree
+	if res.inner != nil && autoFree {
+		runtime.SetFinalizer(res, func(self CedrusValueRef) { self.Free() })
+	}
+}
+
+// Free the underlying C pointer manually.
+func (c CedrusValueRef) Free() {
+	if doubleFreeWarn(&c.freed) {
+		return
+	}
+	C.cedrus_free_vr(c.inner)
+	if c.autoFree {
+		runtime.SetFinalizer(c, nil)
+	}
+}
+
+//// end CedrusValueRef def
+
 type CedrusConfig = C.CedrusConfig
 
-func NewCedrus(dbPath string, config *C.CedrusConfig, truncate bool, autoFree bool) (res Cedrus) {
+func NewCedrus(dbPath string, config *C.CedrusConfig, truncate bool) (res Cedrus) {
 	dbPathStr := C.CString(dbPath)
 	var trunc C.int
 	if truncate {
@@ -70,16 +113,38 @@ func NewCedrus(dbPath string, config *C.CedrusConfig, truncate bool, autoFree bo
 	}
 	res = CedrusFromC(C.cedrus_new(dbPathStr, config, trunc))
 	C.free(rawPtr(dbPathStr))
-	cedrusSetFinalizer(res, autoFree)
+	cedrusSetFinalizer(res, true)
 	return
 }
-
-//// end Cedrus def
 
 func DefaultConfig() CedrusConfig {
 	return C.cedrus_config_default()
 }
 
-func (c Cedrus) Put(key []byte, val []byte) (res int) {
-	return int(C.cedrus_put(c.inner, (*C.uint8_t)(&key[0]), (C.size_t)(len(key)), (*C.uint8_t)(&val[0]), (C.size_t)(len(val))))
+func (c Cedrus) Put(key []byte, val []byte) int {
+	return int(C.cedrus_put(
+		c.inner,
+		(*C.uint8_t)(&key[0]), (C.size_t)(len(key)),
+		(*C.uint8_t)(&val[0]), (C.size_t)(len(val))))
+}
+
+func (c Cedrus) Get(key []byte) (res int, vr CedrusValueRef) {
+	var vr_ *C.CedrusValueRef
+	res = int(C.cedrus_get(c.inner, (*C.uint8_t)(&key[0]), (C.size_t)(len(key)), &vr_))
+	if res == 0 {
+		vr = CedrusValueRefFromC(vr_)
+		cedrusValueRefSetFinalizer(vr, false)
+	}
+	return
+}
+
+func (c Cedrus) Delete(key []byte) int {
+	return int(C.cedrus_delete(
+		c.inner,
+		(*C.uint8_t)(&key[0]), (C.size_t)(len(key))))
+}
+
+func (vr CedrusValueRef) AsBytes() (bytes []byte) {
+	vif := C.cedrus_vr_info(vr.inner)
+	return C.GoBytes(rawPtr(vif.base), C.int(vif.size))
 }
--- example/main.go
+++ example/main.go
@@ -1,11 +1,19 @@
 package main
 
-import "C"
-import "github.com/Determinant/cedrusdb-go"
+import (
+	"fmt"
+	"github.com/Determinant/cedrusdb-go"
+)
 
 func main() {
 	cfg := cedrusdb.DefaultConfig()
-	db := cedrusdb.NewCedrus("./testdb", &cfg, true, false)
-	db.Put([]byte("hello"), []byte("world"))
+	db := cedrusdb.NewCedrus("./testdb", &cfg, true)
+	fmt.Printf("Put = %d\n", db.Put([]byte("hello"), []byte("world")))
+	_, vr := db.Get([]byte("hello"))
+	fmt.Printf("%s\n", string(vr.AsBytes()))
+	vr.Free()
+	fmt.Printf("Put = %d\n", db.Put([]byte("hello1"), []byte("world")))
+	fmt.Printf("Delete = %d\n", db.Delete([]byte("hello1")))
+	fmt.Printf("Delete = %d\n", db.Delete([]byte("hello1")))
 	db.Free()
 }
